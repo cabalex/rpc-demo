2e78ebb43f5cd2853b2536359e6c60ca421da149
cabalex <31020729+cabalex@users.noreply.github.com>
2023-01-06 05:07:11 -0800

    demo

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import VideoCall from './lib/VideoCall/VideoCall.svelte';
+  let myId, theirId;
+  let call = false;
+  let defaults = {video: true, audio: true}
+</script>
+
+<main>
+  {#if !call}
+  <input placeholder="my name" bind:value={myId} type="text"/>
+  <input placeholder="connect to name" bind:value={theirId} type="text" />
+  <button on:click={() => call = true}>call</button>
+  {/if}
+</main>
+
+{#if call}
+  <VideoCall
+    userData={{id: myId}}
+    targetData={{id: theirId}}
+    onHangup={() => call = false}
+    defaults={defaults}
+  />
+{/if}
